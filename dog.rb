c601fc2eefc38e9b4bc6316db4b181bc8e9eefc1
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-19 07:07:50 +0300

    update

---
+++ dog.rb
@@ -0,0 +1,13 @@
+class Dog
+    # setter method
+    def this_dogs_name=(value)
+      @this_dogs_name = value
+    end
+  
+    # getter method
+    def this_dogs_name
+      @this_dogs_name
+    end
+  end
+
+  
