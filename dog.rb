ad69ec83baf8164e0bf546807d43c83b85b9dd99
Charles Onguti <charles.onguti@student.moringaschool.com>
2023-05-19 07:24:47 +0300

    update

--- dog.rb
+++ dog.rb
@@ -1,13 +1,19 @@
 class Dog
-    # setter method
-    def this_dogs_name=(value)
+    # Setter method
+    def name=(value)
       @this_dogs_name = value
     end
   
-    # getter method
-    def this_dogs_name
+    # Getter method
+    def name
       @this_dogs_name
     end
   end
-
+  
+  Lassie = Dog.new
+  Lassie.name = "Lassie"
+  
+  # Call the Dog#name method
+  puts Lassie.name
+  # Output: "Lassie"
   
